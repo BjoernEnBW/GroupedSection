df22f887ca30dddf2042365037fb091bbfcef5dd
Björn Lindner <bjoern.lindner@enbw.com>
2019-09-25 16:57:57 +0200

    initial

--- Sources/GroupedSection/GroupedSection.swift
+++ Sources/GroupedSection/GroupedSection.swift
@@ -1,3 +1,14 @@
-struct GroupedSection {
-    var text = "Hello, World!"
+struct GroupedSection<Identifier: Hashable, Row> {
+    
+    var identifier : Identifier
+    var rows : [Row]
+    
+    static func group(_ rows: [Row], by identifierForRow: (Row) -> Identifier) -> [GroupedSection<Identifier, Row>] {
+        let groups = Dictionary(grouping: rows, by: identifierForRow)
+        
+        return groups.map { (month, headlines) in
+            GroupedSection(identifier: month, rows: headlines)
+        }
+    }
+    
 }
